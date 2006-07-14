e5e92ea8c5983c533435ea42eb23b596d29766da
wrobell <wrobell@1790c4ce-f214-0410-9f43-df667e536b62>
2006-07-14 07:19:45 +0000

    - allow to run all tests

--- trunk/src/danlann/test/__init__.py
+++ trunk/src/danlann/test/__init__.py
@@ -4,3 +4,10 @@ if __debug__:
     logging.basicConfig(level = logging.DEBUG)
 
 
+import unittest
+
+if __name__ == '__main__':
+    from danlann.test.config import *
+    from danlann.test.filemanager import *
+    from danlann.test.parser import *
+    unittest.main()

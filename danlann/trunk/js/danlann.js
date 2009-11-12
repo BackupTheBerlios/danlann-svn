a4891e49e5b95980db57d5303fcfb1f0d8638ed1
wrobell <wrobell@1790c4ce-f214-0410-9f43-df667e536b62>
2009-11-12 20:19:02 +0000

    - allow to navigate albums and photos by pressing appropriate areas of   album/photo pages

--- danlann/trunk/js/danlann.js
+++ danlann/trunk/js/danlann.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var href = $('.navigation .exif a').attr('href');
 
-    //$('body .photo').load(href + ' table');
+    //$('body').load('1.html table.exif')
     $.ajax({type: "GET", url: href, dataType: "xml",
         success: function(xml) {
             table = $(xml).find('table');
@@ -17,5 +17,32 @@ $(document).ready(function() {
         else
             $('table.exif').fadeIn()
         return false;
+    })
+
+    // use page areas for navigation
+    var start_time
+    $('body').mousedown(function() {
+        start_time = new Date().getTime()
+    })
+
+    $('body').mouseup(function(e) {
+        // if click was faster than 300ms, then do nothing
+        if (new Date().getTime() - start_time < 300)
+            return true
+
+        body = $('body')
+        px = e.pageX / body.width()
+
+        goto = null
+        if (0.3 < px && px < 0.6 && e.pageY < 200)
+            goto = $('.navigation a.parent').attr('href')
+        else if (px < 0.4 && e.pageY > 200)
+            goto = $('.navigation a.prev').attr('href')
+        else if (px > 0.6 && e.pageY > 200)
+            goto = $('.navigation a.next').attr('href')
+
+        if (goto != null)
+            window.location = goto
+        
     })
 })
